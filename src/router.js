917671cbef2878b0eae8ffaa014810dcaa57d70c
LookasWasTaken~ <piacen.luca@gmail.com>
2023-06-14 15:32:26 +0200

    ADD router.js and singleproject + apphome views

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+import AppHome from "./views/AppHome.vue";
+import SingleProject from "./views/SingleProject.vue";
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: AppHome,
+    },
+    {
+      path: "/project/:slug",
+      name: "single-project",
+      component: SingleProject,
+    },
+  ],
+});
+export { router };
